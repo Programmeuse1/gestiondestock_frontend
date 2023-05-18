2885a82c2458c4089fca61032355a9eedd3e5571
Ornela2 <noupeyouornela@gmail.com>
2023-05-18 11:06:29 +0100

    up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiqueComponent} from "./pages/page-statistique/page-statistique.component";
+import {PageArticleComponent} from "./pages/articles/page-article/page-article.component";
+import {NouvelArticleComponent} from "./pages/articles/nouvel-article/nouvel-article.component";
+import {PageMvtstkComponent} from "./pages/mvtstk/page-mvtstk/page-mvtstk.component";
+import {PageClientComponent} from "./pages/client/page-client/page-client.component";
+import {PageFournisseurComponent} from "./pages/fournisseur/page-fournisseur/page-fournisseur.component";
+import {NouveauCltFrsComponent} from "./composants/nouveau-clt-frs/nouveau-clt-frs.component";
+import {PageCmdCltFrsComponent} from "./pages/page-cmd-clt-frs/page-cmd-clt-frs.component";
+
+const routes: Routes = [
+  {
+    path:'login',
+    component: PageLoginComponent,
+  },
+  {
+    path:'inscrire',
+    component: PageInscriptionComponent,
+  },
+  {
+    path:'',
+    component: PageDashboardComponent,
+    children: [
+      {
+        path: 'statistique',
+        component: PageStatistiqueComponent
+      },
+      {
+        path: 'articles',
+        component: PageArticleComponent
+      },
+      {
+        path: 'nouvelarticle',
+        component: NouvelArticleComponent
+      },
+      {
+        path: 'mvtstk',
+        component: PageMvtstkComponent
+      },
+      {
+        path: 'clients',
+        component: PageClientComponent
+      },
+      {
+        path: 'nouveauclient',
+        component: NouveauCltFrsComponent
+      },
+      {
+        path: 'commandesclient',
+        component: PageCmdCltFrsComponent
+      },
+      {
+        path: 'fournisseurs',
+        component: PageFournisseurComponent
+      },
+      {
+        path: 'nouveaufournisseur',
+        component: NouveauCltFrsComponent
+      },
+      {
+        path: 'commandesfournisseur',
+        component: PageCmdCltFrsComponent
+      },
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
